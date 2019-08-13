bfc8a8a39bc2663002521435a73dc59c5add45e8
unknown <john_0112@hotmail.com>
2019-08-13 01:47:08 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            if ( i % 2 != 0)
+                continue; //
+                System.out.println("i = " + i);
+               // break; rompe la ejecucion del ciclo
+
+        }
+    }
+}
